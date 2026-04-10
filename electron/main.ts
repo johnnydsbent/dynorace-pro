fbcecad638d131b10f7e159aea5c26e72bd9d44e
johnnydsbent <johnnydsbent@gmail.com>
2026-04-09 22:29:10 -0400

    Fix: correct frontend build path (dist/public) and Electron load path

--- electron/main.ts
+++ electron/main.ts
@@ -29,7 +29,7 @@ function createWindow() {
     mainWindow.loadURL("http://localhost:5000");
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
+    mainWindow.loadFile(path.join(__dirname, "../dist/public/index.html"));
   }
 
   mainWindow.on("closed", () => {
